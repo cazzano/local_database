736bfc6c43416255757989f111ffe480cb7b6db2
cazzano <cazzano.kiddo@gmail.com>
2025-04-10 06:42:34 +0000

    Important

--- stable/backend/python/src/main.py
+++ stable/backend/python/src/main.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+# main.py
+from flask import Flask
+from flask_cors import CORS
+from create_db import initialize_databases
+from routes import setup_routes
+
+app = Flask(__name__)
+CORS(app)
+
+# Initialize the databases when the app starts
+initialize_databases()
+
+# Set up the routes
+setup_routes(app)
+
+if __name__ == "__main__":
+    app.run()  # This will run on port 5000
